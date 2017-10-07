edf3e2fc30e1621eb098bbbdb691a26d252ead66
Arkarr <Arkarr@users.noreply.github.com>
2017-10-07 22:54:36 +0200

    Small fix
    Small fix to prevent ASteambot from crashing will processing client connection message.

--- SOURCE/ASteambot/Networking/GameServerRequest.cs
+++ SOURCE/ASteambot/Networking/GameServerRequest.cs
@@ -18,8 +18,17 @@ namespace ASteambot.Networking
         public GameServerRequest(Socket socket, string srvid, string code, string args)
         {
             string[] idmid = srvid.Split(',');
-            this.ServerID = Int32.Parse(idmid[0]);
-            this.ModuleID = Int32.Parse(idmid[1]);
+
+            Console.WriteLine("Trying to parse " + idmid[0] + " to Int32 !");
+
+            int tmpInt;
+            Int32.TryParse(idmid[0], out tmpInt);
+            ServerID = tmpInt;
+
+            Console.WriteLine("Trying to parse " + idmid[1] + " to Int32 !");
+            Int32.TryParse(idmid[1], out tmpInt);
+            ModuleID = tmpInt;
+
             this.NetworkCode = Int32.Parse(code);
             this.Arguments = args;
             this.Socket = socket;
