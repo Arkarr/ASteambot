270210e9c19c27c20fc192c50b313298e9697b75
Arkarr <Arkarr@users.noreply.github.com>
2017-11-12 15:42:34 +0100

    2.7.0 - Trade value bug
    Fixed a major bug where the trade value was divided by 100 !

--- SOURCE/ASteambot/Program.cs
+++ SOURCE/ASteambot/Program.cs
@@ -18,7 +18,7 @@ namespace ASteambot
         private static Manager steambotManager;
         private static Thread threadManager;
 
-        private static string BUILD_VERSION = "2.6.0 - PUBLIC";
+        private static string BUILD_VERSION = "2.7.0 - PUBLIC";
 
         public static bool DEBUG;
 
