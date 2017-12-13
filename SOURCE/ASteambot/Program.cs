097ab694fb319c1fa1b03f3c7a03eda036b11d37
Arkarr <Arkarr@users.noreply.github.com>
2017-12-13 11:45:57 +0100

    V 2.9.3 - Fixed EXEC bug (hopefully)

--- SOURCE/ASteambot/Program.cs
+++ SOURCE/ASteambot/Program.cs
@@ -18,7 +18,7 @@ namespace ASteambot
         private static Manager steambotManager;
         private static Thread threadManager;
 
-        private static string BUILD_VERSION = "2.9.2 - PUBLIC";
+        private static string BUILD_VERSION = "2.9.3 - PUBLIC";
 
         public static bool DEBUG;
 
