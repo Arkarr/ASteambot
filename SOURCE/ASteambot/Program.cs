6bbda154dac1a18743075944aa7495fd195e8940
Arkarr <Arkarr@users.noreply.github.com>
2018-03-16 18:16:47 +0100

    V 2.9.96 - Socket fix
    Fixed some issue where the socket connection was lost on server reboot.

--- SOURCE/ASteambot/Program.cs
+++ SOURCE/ASteambot/Program.cs
@@ -18,7 +18,7 @@ namespace ASteambot
         private static Manager steambotManager;
         private static Thread threadManager;
 
-        private static string BUILD_VERSION = "2.9.95 - PUBLIC";
+        private static string BUILD_VERSION = "2.9.96 - PUBLIC";
 
         public static bool DEBUG;
 
