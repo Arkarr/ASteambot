66a98b51ee995ab9f1ca389f2ba2a1fd71cc1e00
Arkarr <mrarkarr@gmail.com>
2018-06-06 21:52:07 +0200

    V 3.7.3 - Reconnection fix
    Fixed a bug where ASteambot wasn't reconnecting correctly on connection loose.

--- SOURCE/ASteambot/Program.cs
+++ SOURCE/ASteambot/Program.cs
@@ -22,7 +22,7 @@ namespace ASteambot
         private static Thread threadManager;
         private static Translation.Translation translation;
 
-        private static string BUILD_VERSION = "3.7.2 - PUBLIC";
+        private static string BUILD_VERSION = "3.7.3 - PUBLIC";
 
         public static bool DEBUG;
 
