da4aee1fd000d3a7fe175babf6f596fc32f2e237
Arkarr <mrarkarr@gmail.com>
2018-06-07 21:39:04 +0200

    test
    test

--- SOURCE/ASteambot/Program.cs
+++ SOURCE/ASteambot/Program.cs
@@ -26,7 +26,7 @@ namespace ASteambot
         private static Thread threadManager;
         private static Translation.Translation translation;
         
-        private static string BUILD_VERSION = "3.9 - PUBLIC";
+        private static string BUILD_VERSION = "3.9.1 - PUBLIC";
 
         public static bool DEBUG;
 
