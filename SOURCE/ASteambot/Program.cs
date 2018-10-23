39ec606fc621c01281328c6e3e94111af304a962
Arkarr <mrarkarr@gmail.com>
2018-10-23 22:56:24 +0200

    V 5.5 - Quick "fix" for the updater not working on some Linux host.

--- SOURCE/ASteambot/Program.cs
+++ SOURCE/ASteambot/Program.cs
@@ -32,7 +32,7 @@ namespace ASteambot
         private static Manager steambotManager;
         private static Thread threadManager;
         
-        private static string BUILD_VERSION = "5.4 - PUBLIC";
+        private static string BUILD_VERSION = "5.5 - PUBLIC";
 
         public static bool DEBUG;
 
