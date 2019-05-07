1c6f45a8c30bcfc6e0588a3d2b1d0d3a10a4eb71
Arkarr <mrarkarr@gmail.com>
2019-05-07 08:33:33 +0200

    Preparing V10.0

---
+++ SOURCE/ASteambot_TestModules/ASteambot_ChatTest.cs
@@ -0,0 +1,18 @@
+﻿using ASteambot.Interfaces;
+using SteamKit2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASteambot_TestModules
+{
+    public class ASteambot_ChatTest : ISteamChatHandler
+    {
+        public void HandleMessage(SteamID partenar, string message)
+        {
+            Console.WriteLine(partenar + " wrote me a message : '" + message + "' !");
+        }
+    }
+}
