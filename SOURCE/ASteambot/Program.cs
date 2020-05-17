2d5e1ae9b09a7e9c133e0bfd75fe07c7b856c00c
Arkarr <mrarkarr@gmail.com>
2020-05-17 20:34:31 +0200

    V11.7 - Fixed trader offer value being set to 0

--- SOURCE/ASteambot/Program.cs
+++ SOURCE/ASteambot/Program.cs
@@ -25,7 +25,7 @@ namespace ASteambot
         private static Manager steambotManager;
         private static Thread threadManager;
 
-        private static string BUILD_VERSION = "V11.6";
+        private static string BUILD_VERSION = "V11.7";
         private static string BUILD_NAME = BUILD_VERSION + " - PUBLIC";
 
         public static bool DEBUG;
